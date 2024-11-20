c7bab2d5057700976bec0b5a3478365376217123
Endrit Kaskija <endrit.kaskija@gmail.com>
2024-11-20 18:41:12 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.js", // Se usi JS/JSX/Vue
+        "./resources/js/**/*.{vue,js}", // Include anche file Vue.js
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms],
+};
